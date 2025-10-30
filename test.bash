0343d5837e8d728cf17528d02c671b51f10f1dfe
Morofuji Junko <mimosa4kai@gmail.com>
2025-10-30 14:49:55 +0900

    Update test.bash with debug mode and strange input tests

--- test.bash
+++ test.bash
@@ -1,24 +1,28 @@
 #!/bin/bash -xv
-###スライド23のやつ###
+# SPDX-FileCopyrightText: 2025 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
+
+###スライド24のやつ###
 ng (){
 	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-	
+
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
 [ "${out}" = 15 ] || ng "$LINENO"
-+ '[' 15 = 15 ']'
 
-[ "${res}" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
-+ exit 0
-
-
