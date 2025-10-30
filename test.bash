bca399b87b345a6db15a3961e4494d6656d110e2
Morofuji Junko <mimosa4kai@gmail.com>
2025-10-30 13:59:40 +0900

    Enable debug mode (-xv) in test.bash for trace output

--- test.bash
+++ test.bash
@@ -1,16 +1,24 @@
-#!/bin/bash
-
+#!/bin/bash -xv
+###スライド23のやつ###
 ng (){
 	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-
+	
 out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ out=15
 [ "${out}" = 15 ] || ng "$LINENO"
++ '[' 15 = 15 ']'
 
 [ "${res}" = 0 ] && echo OK
++ '[' 0 = 0 ']'
++ echo OK
+OK
 exit $res
++ exit 0
 
 
